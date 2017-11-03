7b9113484f0b7a0b4ee546c14e43bd5b59d9008a
@mdawg85 <margaretshelaghmcgovern@gmail.com>
2017-11-03 15:07:42 -0700

    display data from firebase on welcome page

--- discussion-forum/src/app/welcome/welcome.component.ts
+++ discussion-forum/src/app/welcome/welcome.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { Post } from '../post.model';
+import { Router } from '@angular/router';
+import { PostService } from '../post.service';
+import { FirebaseListObservable } from 'angularfire2/database';
 
 @Component({
   selector: 'app-welcome',
   templateUrl: './welcome.component.html',
-  styleUrls: ['./welcome.component.css']
+  styleUrls: ['./welcome.component.css'],
+  providers: [PostService]
 })
 export class WelcomeComponent implements OnInit {
+  posts: FirebaseListObservable<any[]>;
+  currentRoute: string = this.router.url;
 
-  constructor() { }
+  constructor(private router: Router, private postService: PostService) { }
 
   ngOnInit() {
+    this.posts = this.postService.getPosts();
   }
-
 }
