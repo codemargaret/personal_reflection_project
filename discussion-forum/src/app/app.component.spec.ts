486b5913669aa5e180d260a1bd62a15903bb3e55
@mdawg85 <margaretshelaghmcgovern@gmail.com>
2017-11-03 11:21:07 -0700

    add test to detect title change in app component, modify app component test environment to include forEach

--- discussion-forum/src/app/app.component.spec.ts
+++ discussion-forum/src/app/app.component.spec.ts
@@ -1,8 +1,14 @@
-import { TestBed, async } from '@angular/core/testing';
-
+import { TestBed, async, ComponentFixture } from '@angular/core/testing';
+import { By }              from '@angular/platform-browser';
+import { DebugElement }    from '@angular/core';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+  let de:      DebugElement;
+  let el:      HTMLElement;
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
@@ -11,22 +17,30 @@ describe('AppComponent', () => {
     }).compileComponents();
   }));
 
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance; // WelcomeComponent test instance
+    fixture.detectChanges();
+  });
+
   it('should create the app', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
 
   it(`should have as title 'app works!'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app.title).toEqual('app works!');
   }));
 
   it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
     expect(compiled.querySelector('h1').textContent).toContain('app works!');
   }));
+
+  it('should display a different test title', () => {
+    const app = fixture.debugElement.componentInstance;
+    component.title = 'Test Title';
+    expect(app.title).toContain('Test Title');
+  });
 });
--- discussion-forum/src/app/welcome/welcome.component.spec.ts
+++ discussion-forum/src/app/welcome/welcome.component.spec.ts
@@ -1,22 +1,30 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By }              from '@angular/platform-browser';
+import { DebugElement }    from '@angular/core';
 
 import { WelcomeComponent } from './welcome.component';
 
 describe('WelcomeComponent', () => {
   let component: WelcomeComponent;
   let fixture: ComponentFixture<WelcomeComponent>;
+  let de:      DebugElement;
+  let el:      HTMLElement;
 
-  beforeEach(async(() => {
+  beforeEach(async(() => { //needs to be placed in async, different from tutorial
     TestBed.configureTestingModule({
-      declarations: [ WelcomeComponent ]
+      declarations: [ WelcomeComponent ] // declare the test component
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(WelcomeComponent);
-    component = fixture.componentInstance;
+    component = fixture.componentInstance; // WelcomeComponent test instance
     fixture.detectChanges();
+
+    // query for the title <h1> by CSS element selector, //part of tutorial test, but perhaps not necessary for separated components?
+    // de = fixture.debugElement.query(By.css('h1'));
+    // el = de.nativeElement;
   });
 
   it('should create', () => {
